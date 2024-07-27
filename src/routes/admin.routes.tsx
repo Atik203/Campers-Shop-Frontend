d8056d614d60764c305aa5b4a20eb49bce33f0ec
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-27 18:41:31 +0600

    added icons to dashboard

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,4 +1,9 @@
-import { HomeIcon, StarIcon } from "@heroicons/react/24/solid";
+import {
+  Cog6ToothIcon,
+  HomeIcon,
+  PlusCircleIcon,
+  TableCellsIcon,
+} from "@heroicons/react/24/solid";
 import AddProduct from "../pages/AddProduct";
 import AllProducts from "../pages/AllProducts";
 import Dashboard from "./../pages/Dashboard";
@@ -11,18 +16,19 @@ export const adminPaths = [
   },
   {
     name: "Products Management",
-    icon: <HomeIcon className="h-6 w-6" />,
+    icon: <Cog6ToothIcon className="h-6 w-6" />,
     children: [
       {
         name: "Add Product",
         path: "add-product",
         element: <AddProduct />,
-        icon: <StarIcon className="h-6 w-6" />,
+        icon: <PlusCircleIcon className="h-6 w-6" />,
       },
       {
         name: "All Products",
         path: "all-products",
         element: <AllProducts />,
+        icon: <TableCellsIcon className="h-6 w-6" />,
       },
     ],
   },
