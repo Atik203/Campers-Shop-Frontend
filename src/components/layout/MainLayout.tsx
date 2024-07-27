53f7011a111f1a270c089d4ae9da170bc4b1a41a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-27 17:24:26 +0600

    added footer

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../ui/Footer";
 import Navbar from "../ui/Navbar";
 
 const MainLayout = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <Navbar />
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-6xl mx-auto min-h-screen">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,7 @@ module.exports = {
   },
   plugins: [
     require("tailwindcss-animate"),
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'), 
+    require('@tailwindcss/aspect-ratio'),
   ],
 };
