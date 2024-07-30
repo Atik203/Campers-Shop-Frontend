5230a5d571113aa3dfbc4d3f38e1d32a2fa9ce80
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-31 03:23:10 +0600

    update confirm page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -535,7 +535,7 @@ export default function Checkout() {
             <div className="mt-10">
               <button
                 type="submit"
-                className="w-full bg-indigo-600 border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="w-full bg-primary border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Place order
               </button>
