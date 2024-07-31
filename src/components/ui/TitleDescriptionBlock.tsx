97d56ee599e3bfece2b04bfe12e85cf82bac4a45
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-31 19:37:45 +0600

    updated categories section

--- src/components/ui/TitleDescriptionBlock.tsx
+++ src/components/ui/TitleDescriptionBlock.tsx
@@ -11,10 +11,10 @@ const TitleDescriptionBlock: React.FC<BlockProps> = ({
   return (
     <div className="grid grid-cols-12 my-4 md:mb-12">
       <div className="col-span-12 lg:col-span-6 lg:col-start-4 text-center">
-        <h2 className="text-3xl text-black leading-none md:text-[45px] font-bold mb-6">
+        <h2 className="text-2xl md:text-4xl text-black leading-none  font-bold mb-6">
           {title}
         </h2>
-        <p className="text-lg w-11/12 md:w-10/12 mx-auto">{description}</p>
+        <p className="text-base w-11/12 md:w-10/12 mx-auto">{description}</p>
       </div>
     </div>
   );
